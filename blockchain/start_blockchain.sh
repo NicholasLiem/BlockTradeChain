3d0b1ab5d3745f52a54a74b4072d1efb9093d9b2
Nicholas Liem <13521135@std.stei.itb.ac.id>
2024-12-28 23:38:12 +0700

    add back config and scripts

---
+++ blockchain/start_blockchain.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DATA_DIR="./blockchainData"
+
+ETHERBASE=36f09de68ce7e1cac91222205de80529e39083b7
+
+if [ -z "$ETHERBASE" ]; then
+  echo "Error: No accounts found. Please create an account before starting the miner."
+  exit 1
+fi
+
+echo "Using etherbase: $ETHERBASE"
+
+echo "Initializing blockchain with genesis.json..."
+geth --datadir $DATA_DIR init genesis.json
+
+echo "Starting the private blockchain..."
+geth --datadir $DATA_DIR \
+     --networkid 4785 \
+     --http \
+     --http.addr "0.0.0.0" \
+     --http.port 8545 \
+     --http.api "personal,eth,net,web3" \
+     --http.corsdomain "*" \
+     --allow-insecure-unlock \
+     --mine \
+     --miner.threads=1 \
+     --miner.etherbase="$ETHERBASE" \
+     --unlock "0x$ETHERBASE" \
+     --password "./password.txt" \
+     --preload "startmine.js"
